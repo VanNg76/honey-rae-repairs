227c9b4135d14c988dbc454e9b08c727e7cf61a3
Van Nguyen <thanh_xer@yahoo.com>
2022-02-24 11:11:39 -0600

    finish chapter 15

--- src/components/serviceTickets/TicketList.js
+++ src/components/serviceTickets/TicketList.js
@@ -27,9 +27,21 @@ export const TicketList = () => {
             } else {
                 showMessage(`You have ${tickets.length} tickets`)
             }
+
         },
         [tickets]
     )
+
+    const deleteTicket = (id) => {
+        fetch(`http://localhost:8088/serviceTickets/${id}`, {
+            method: "DELETE"
+        })
+
+        // update tickets array to eliminate the deleted one!
+        const copy = tickets.filter(ticket => ticket.id != id)
+        setTickets(copy)
+        
+    }
 
     return (
         <>
@@ -43,6 +55,10 @@ export const TicketList = () => {
                     return <div key={`ticket--${ticket.id}`}>
                                 <p className={ticket.emergency ? "emergency" : ""}>
                                     {ticket.emergency ? "🚑" : ""} <Link to={`/serviceTickets/${ticket.id}`}>{ticket.description}</Link>
+                                    <button onClick={() => {
+                                        deleteTicket(ticket.id)
+                                    }}
+                                    >Delete</button>
                                 </p>
                             </div>
                 })
